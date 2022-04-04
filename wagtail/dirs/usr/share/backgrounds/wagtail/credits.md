4ad1b0d7a38a1805f906cfdc6206e1b96411b0c2
Piero Proietti <piero.proietti@gmail.com>
2022-04-04 17:09:38 +0200

    added waitail background

---
+++ wagtail/dirs/usr/share/backgrounds/wagtail/credits.md
@@ -0,0 +1,7 @@
+# Credits
+
+![image](./50014314876_01f0cfd8e2_k.jpg)
+author: [Sæmundur Valdimarsson](https://www.flickr.com/photos/163930705@N05/)
+origin: [link](https://www.flickr.com/photos/163930705@N05/50014314876)
+
+
--- wagtail/gnome_background_set.sh
+++ wagtail/gnome_background_set.sh
@@ -1,1 +1,1 @@
-gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/waydroid/BG_1.png"
+gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/wagtail/50014314876_01f0cfd8e2_k.jpg"
