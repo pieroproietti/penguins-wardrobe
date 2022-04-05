75951826f015e599df8666e9ae2f65d1bd7b988c
Piero Proietti <piero.proietti@gmail.com>
2022-04-05 13:12:43 -0400

    owl

--- owl/gnome_background_set.sh
+++ owl/gnome_background_set.sh
@@ -1,2 +1,2 @@
-gsettings set org.gnome.desktop.background picture-uri "file:////usr/share/backgrounds/colibri-wallpapers/3794764350_2839ca0b26_b.jpg"
-
+#gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/owl/37161836845_af56317fda_h.jpg"
+xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/owl/37161836845_af56317fda_h.jpg
