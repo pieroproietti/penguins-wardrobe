6ccbd5f8ba75be68d2cadebf16a2a08dfff2d88e
artisan <piero.proietti@gmai.com>
2022-04-07 11:46:51 -0400

    eagle take form

---
+++ eagle/xfcon_background_set.sh
@@ -0,0 +1,2 @@
+#gsettings set org.gnome.desktop.background picture-uri "file:////usr/share/backgrounds/colibri/3794764350_2839ca0b26_b.jpg"
+xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/owl/37161836845_af56317fda_h.jpg
