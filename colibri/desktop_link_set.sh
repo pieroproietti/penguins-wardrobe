8f04b6cb659e1aa4964d096f003a49ddc175943f
artisan <piero.proietti@gmail.com>
2022-04-09 09:17:51 +0200

    colibri desktop_background_set.sh desktop_links_set.sh

---
+++ colibri/desktop_link_set.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cp /usr/lib/penguins-eggs/addons/eggs/adapt/applications/eggs-adapt.desktop ~/Desktop
+cp /usr/lib/penguins-eggs/assets/penguins-eggs.desktop ~/Desktop
+cp /usr/lib/penguins-eggs/assets/eggs.png /usr/share/iconss
+
+
+
+
