1a705981f933fe6b0eb2da91c9bbec73ff6168fd
artisan <piero.proietti@gmail.com>
2022-04-11 15:38:19 +0200

    desktop_link_set

--- colibri/desktop_link_set.sh
+++ colibri/desktop_link_set.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# $1 user
-# $2 desktop
+USER=$1
+DESKTOP=$2
+
+# create DESKTOP
+if [ -d "$DESKTOP" ]; then
+    mkdir $DESKTOP -p
+fi
 
 # copy icons
+
 cp /usr/lib/penguins-eggs/assets/eggs.png /usr/share/icons
 
 # copy links
-cp /usr/lib/penguins-eggs/addons/eggs/adapt/applications/eggs-adapt.desktop $2
-cp /usr/lib/penguins-eggs/assets/penguins-eggs.desktop $2
-chown $1:$1 $2 -R
+cp /usr/lib/penguins-eggs/addons/eggs/adapt/applications/eggs-adapt.desktop $DESKTOP
+cp /usr/lib/penguins-eggs/assets/penguins-eggs.desktop $DESKTOP
+chown $USER:$USER $DESKTOP -R
