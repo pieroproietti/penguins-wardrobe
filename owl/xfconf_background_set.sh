134c967e2a429537e3e86f34f527606a4b9555d2
artisan <piero.proietti@gmail.com>
2022-04-11 09:36:39 +0200

    rimosso

--- owl/xfconf_background_set.sh
+++
@@ -1,2 +0,0 @@
-#gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/owl/37161836845_af56317fda_h.jpg"
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/owl/37161836845_af56317fda_h.jpg
