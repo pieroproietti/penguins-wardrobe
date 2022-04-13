e649d4f92dd9443e528d85aaf079f827e75eab75
Piero Proietti <piero.proietti@gmail.com>
2022-04-13 10:50:58 +0200

    modificate immagini a 18.1

--- warbier/waydroid/install_image_from_local.sh
+++ warbier/waydroid/install_image_from_local.sh
@@ -2,6 +2,5 @@
 
 mkdir -p /usr/share/waydroid-extra/images
 read -p "Installing system.img and vendor.img, press enter to continue"
-scp artisan@192.168.61.2:/eggs/*.img /usr/share/waydroid-extra/images
+scp artisan@192.168.61.2:/opt/waydroid/18.1/*.img /usr/share/waydroid-extra/images
 waydroid init -f
-
