8f939f915a7902890808583cb0d0da8e35cbcf21
Piero Proietti <piero.proietti@gmail.com>
2022-04-15 12:17:41 +0200

    autologin

--- owl/lightdm_autologin_set.sh
+++ owl/lightdm_autologin_set.sh
@@ -8,5 +8,5 @@ BACKGROUND="/usr/share/backgrounds/owl/37161836845_af56317fda_h.jpg"
 # set autologin
 echo autologin-user=${USER} >> /etc/lightdm/lightdm.conf
 
-# set backgroud
-echo backgroud=${BACKGROUND} >> /etc/lightdm/lightdm-gtk-greeter.conf
+# set background
+echo background=${BACKGROUND} >> /etc/lightdm/lightdm-gtk-greeter.conf
