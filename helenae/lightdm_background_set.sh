110dc8d431db158a8c0be9ca459cad279fe2c180
Piero Proietti <piero.proietti@gmail.com>
2022-04-16 10:52:57 +0200

    echo -e errato

--- helenae/lightdm_background_set.sh
+++ helenae/lightdm_background_set.sh
@@ -7,7 +7,7 @@ BACKGROUND="/usr/share/backgrounds/helenae/25759889846_911ba6c1fd_o.jpg"
 
 # set autologin
 # We need some different to just append 
-echo -e "# wardrobe\n[Seat:*]\nautologin-user=${USER}" >> /etc/lightdm/lightdm.conf
+echo "# wardrobe\n[Seat:*]\nautologin-user=${USER}" >> /etc/lightdm/lightdm.conf
 
 # set background
-echo -e "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
