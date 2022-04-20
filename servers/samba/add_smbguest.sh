1060fca9ef9ae83c195f209f96c9630dc85063dd
Piero Proietti <piero.proietti@gmail.com>
2022-04-20 18:39:51 +0200

    samba

--- servers/samba/add_smbguest.sh
+++ servers/samba/add_smbguest.sh
@@ -3,9 +3,17 @@ groupadd smbguest
 useradd -g smbguest -d /dev/null -s /bin/false smbguest
 # add group smbuser for samba
 groupadd smbuser
+# umask 077
+mkdir -p /etc/samba/private
+touch /etc/samba/private/smbpasswd
+# adding group smbuser to user
+usermod -aG smbuser $SUDO_USER
+# creating sample users
 useradd -d /home/argo -g smbuser -s /bin/false -m argo
+echo argo:evolution | chpasswd
 smbpasswd -a argo
 useradd -d /home/profes -g smbuser -s /bin/false -m profes
+echo profes:evolution | chpasswd
 smbpasswd -a profes
 groupadd workstation
 useradd -d /dev/null -g workstation -s /bin/false samba
