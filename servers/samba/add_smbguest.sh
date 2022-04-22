7534c772032ae6462dbda5a8b87a1431be014bd4
artisan <piero.proietti@gmail.com>
2022-04-22 07:43:08 +0200

    samba

--- servers/samba/add_smbguest.sh
+++ servers/samba/add_smbguest.sh
@@ -1,21 +1,28 @@
-# add group smbguest for samba
-groupadd smbguest
-useradd -g smbguest -d /dev/null -s /bin/false smbguest
-# add group smbuser for samba
-groupadd smbuser
+# create /etc/samba/private/smbpasswd
 # umask 077
 mkdir -p /etc/samba/private
 touch /etc/samba/private/smbpasswd
-# adding group smbuser to user
+
+# add groups for samba
+groupadd smbguest
+groupadd smbuser
+groupadd workstation
+
+# add user smdguest for samba
+useradd -g smbguest -d /dev/null -s /bin/false smbguest
+
+# adding user $SUDO_USER to group smbuser
 usermod -aG smbuser $SUDO_USER
+
 # creating sample users
 # useradd -d /home/argo -g smbuser -s /bin/false -m argo
 # echo argo:evolution | chpasswd
-# smbpasswd -a argo
+# echo argo:evolution | smbpasswd -a argo
 # useradd -d /home/profes -g smbuser -s /bin/false -m profes
 # echo profes:evolution | chpasswd
-# smbpasswd -a profes
-#groupadd workstation
-#useradd -d /dev/null -g workstation -s /bin/false samba
-#smbpasswd -a -m samba
+# echo argo:evolution |smbpasswd -a profes
+
+# adding workstation
+# useradd -d /dev/null -g workstation -s /bin/false samba
+# smbpasswd -a -m samba
 
