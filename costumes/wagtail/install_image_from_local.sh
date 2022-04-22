dcc29e881fc3087b97593246eafa2a67a0e309ff
Piero Proietti <piero.proietti@gmail.com>
2022-04-22 12:15:44 +0200

    going back to solve...

--- costumes/wagtail/install_image_from_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p /usr/share/waydroid-extra/images
-read -p "Installing system.img and vendor.img, press enter to continue"
-scp artisan@192.168.61.2:/opt/waydroid/18.1/*.img /usr/share/waydroid-extra/images
-waydroid init -f
