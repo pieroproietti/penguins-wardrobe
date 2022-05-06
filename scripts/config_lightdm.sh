a8fb5b10168420afaa51e48dead1cda4d57bb640
Piero Proietti <piero.proietti@gmail.com>
2022-05-06 18:27:55 +0200

    helenae

--- scripts/config_lightdm.sh
+++ scripts/config_lightdm.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-USER=$1
-DESKTOP=$2
-#BACKGROUND="/usr/share/backgrounds/helenae/25759889846_911ba6c1fd_o.jpg"
+#USER=$1
+#DESKTOP=$2
+USER=${SUDO_USER}
+DESKTOP=${EGGS_DESKTOP}
+BACKGROUND=${EGGS_BACKGROUND}
 
 # set autologin
 # We need some different to just append 
 echo "# wardrobe\n[Seat:*]\nautologin-user=${USER}" >> /etc/lightdm/lightdm.conf
 
 # set background
-#echo "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
