6957b81619f51332feb4d0066eceae21789f7740
Piero Proietti <piero.proietti@gmail.com>
2022-05-10 08:27:15 +0200

    rit

--- scripts/config_lightdm.sh
+++ scripts/config_lightdm.sh
@@ -4,7 +4,7 @@ COSTUME=$1
 BACKGROUND=$(ls /usr/share/backgrounds/"${COSTUME}"/*.jpg)
 
 # autologin
-echo -e "# wardrobe\n[Seat:*]\nautologin-user=${SUDO_USER}" >> /etc/lightdm/lightdm.conf
+echo -e '# wardrobe\n[Seat:*]\nautologin-user="${SUDO_USER}"` >> /etc/lightdm/lightdm.conf
 
 # background
 echo -e "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
