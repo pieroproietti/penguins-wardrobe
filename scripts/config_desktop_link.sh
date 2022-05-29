400e1d00ecb1677dedf2294f8251813028e1576f
Piero Proietti <piero.proietti@gmail.com>
2022-05-29 07:01:28 +0200

    adapt

--- scripts/config_desktop_link.sh
+++ scripts/config_desktop_link.sh
@@ -13,5 +13,6 @@ cp /usr/lib/penguins-eggs/assets/eggs.png /usr/share/icons
 # copy links
 cp /usr/lib/penguins-eggs/addons/eggs/adapt/applications/eggs-adapt.desktop "${DESKTOP}"
 cp /usr/lib/penguins-eggs/addons/eggs/adapt/bin/adapt /usr/local/bin/adapt
+chmod +x /usr/local/bin/adapt
 cp /usr/lib/penguins-eggs/assets/penguins-eggs.desktop "${DESKTOP}"
 chown "${SUDO_USER}":"${SUDO_USER}" "${DESKTOP}" -R
