be1b0621bddbaad7beaf456a7175a817461e9a53
Piero Proietti <piero.proietti@gmail.com>
2022-07-05 16:00:14 +0200

    restyling...

--- costumes/colibri/manjaro-colibri.sh
+++ costumes/colibri/manjaro-colibri.sh
@@ -1,5 +1,15 @@
-# here we define a colibri for manjaro
-# I need experience here
+#
+# temporary script to get colibri in manjaro
+# 
+
+clear
+COSTUME="colibri"
+MY_USERNAME=`logname`
+echo "MY_USERNAME: ${MY_USERNAME}"
+
+MY_USERHOME=/home/${MY_USERNAME}
+echo "MY_USERHOME: ${MY_USERHOME}"
+
 pacman -Syu \
 libxfce4ui-gtk3 \
 lightdm \
@@ -22,35 +32,27 @@ xfwm4 \
 xdg-user-dirs \
 xorg-server 
 
-pamac spice-vdagent \
-network-manager-applet
+pacman -Syu  spice-vdagent network-manager-applet
 
 # eggs-dev
-pacman -Syu install vscode nodejs npm pnpm
+pacman -Syu vscode nodejs npm 
+npm install pnpm -g
 
 systemctl enable NetworkManager
 systemctl enable lightdm
 
-clear
-
-# SUDO_USER=esiste
-COSTUME="colibri"
-MY_USERNAME=`logname`
-echo "MY_USERNAME: ${MY_USERNAME}"
-
-MY_USERHOME=/home/${MY_USERNAME}
-echo "MY_USERHOME: ${MY_USERHOME}"
 
 # copy configuration from dirs
 cp ./dirs/* / -R
-echo "rsync -avx ./dirs/etc/skel/.config ${MY_USERHOME}/.config"
+
+# copy configuration from dirs to MY_USERHOME
 rsync -avx ./dirs/etc/skel/.config ${MY_USERHOME}/.config
 
-echo "../../scripts/config_lightdm.sh colibri ${MY_USERHOME}"
-../../scripts/config_lightdm.sh colibri ${MY_USERHOME}
+# config ligghtdn $COSTUME $MY_USERHOME
+../../scripts/config_lightdm.sh ${COSTUME} ${MY_USERHOME}
 
-echo "../../scripts/config_desktop_link.sh ${MY_USERHOME}"
+# config desktop links $MY_USERHOME
 ../../scripts/config_desktop_link.sh ${MY_USERHOME}
 
-echo "chown ${MY_USERNAME}:${MY_USERNAME} ${MY_USERHOME} -R"
+# Reimpostazione diritti
 chown ${MY_USERNAME}:${MY_USERNAME} ${MY_USERHOME} -R
