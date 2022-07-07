3e63f43038f736a5d07b75f8ab9380403e8c631e
Piero Proietti <piero.proietti@gmail.com>
2022-07-07 07:49:21 +0200

    lightdm

--- scripts/config_lightdm.sh
+++ scripts/config_lightdm.sh
@@ -10,5 +10,12 @@ if [ -f /etc/lightdm/lightdm.conf.d/lightdm-autologin-greeter.conf ]; then
 fi
 echo -e "# wardrobe\n[Seat:*]\nautologin-user=${SUDO_USER}" >> ${LIGHTDM_CONF}
 
+# greeter-session
+echo -e "greeter-session=lightdm-gtk-greeter" >> ${LIGHTDM_CONF}
+
+# session
+echo -e "user-session=xfce" >> ${LIGHTDM_CONF}
+
 # background
-echo -e "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo -e "wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
+
