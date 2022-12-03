f6f1da381ecd38ccc60bbd926ba5b3263becdffa
Piero Proietti <piero.proietti@gmail.com>
2022-12-03 09:16:50 +0100

    colibri arch

--- costumes/colibri/archlinux-colibri.sh
+++ costumes/colibri/archlinux-colibri.sh
@@ -12,18 +12,18 @@ MY_USERHOME="/home/${MY_USERNAME}"
 echo "MY_USERHOME: ${MY_USERHOME}"
 
 pacman -Syyu
-pacman -S xorg-server xorg-apps
-pacman -S lightdm lightdm-gtk-greeter
-pacman -S xfce4 xfce4-goodies
-pacman -S spice-vdagent shellcheck xdg-user-dirs
+pacman -Syy xorg-server xorg-apps
+pacman -Syy lightdm lightdm-gtk-greeter
+pacman -Syy xfce4 xfce4-goodies
+pacman -Syy spice-vdagent shellcheck xdg-user-dirs
 # tools
-pacman -S xarchiver unzip firefox network-manager-applet polkit-gnome
+pacman -Syy xarchiver unzip firefox network-manager-applet polkit-gnome
 
 # pongo utente nel gruppo autologin
 gpasswd -a ${MY_USERNAME} autologin
 
 # eggs-dev
-pacman -Syu vscode nodejs npm 
+pacman -Syyu vscode nodejs npm 
 npm install pnpm -g
 
 systemctl enable NetworkManager
