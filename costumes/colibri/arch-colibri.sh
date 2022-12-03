211e29fb3912c72f9bf650ed6710765243bc75ea
Piero Proietti <piero.proietti@gmail.com>
2022-12-03 10:43:57 +0100

    working on arch

--- costumes/colibri/arch-colibri.sh
+++ costumes/colibri/arch-colibri.sh
@@ -53,3 +53,17 @@ rsync -avx ./dirs/etc/skel/.config "${MY_USERHOME}"/
 
 # Reimpostazione diritti
 chown "${MY_USERNAME}:${MY_USERNAME}" "${MY_USERHOME}" -R
+
+echo $COSTUME /etc/hostname
+cat <<'EOF' >> /etc/hosts
+127.0.0.1 localhost localhost.localdomain
+127.0.1.1 ${COSTUME}
+# The following lines are desirable for IPv6 capable hosts
+:: 1     ip6 - localhost ip6 - loopback
+fe00:: 0 ip6 - localnet
+ff00:: 0 ip6 - mcastprefix
+ff02:: 1 ip6 - allnodes
+ff02:: 2 ip6 - allrouters
+ff02:: 3 ip6 - allhosts
+EOF
+
