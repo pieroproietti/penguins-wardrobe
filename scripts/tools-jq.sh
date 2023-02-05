69244afca6b8935008d0031be8cb675b5fc826bb
Piero Proietti <piero.proietti@gmail.com>
2023-02-05 08:37:40 +0100

    wagtail

--- scripts/tools-jq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# yq
-VERSION=v4.2.0
-BINARY=yq_linux_amd64
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - |\
-tar xz && mv ${BINARY} /usr/bin/yq
-
