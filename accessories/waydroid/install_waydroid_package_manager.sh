d6621d16013eff0991694b9c3ba63b4d5f59003e
Piero Proietti <piero.proietti@gmail.com>
2023-02-05 11:52:50 +0100

    ====

--- accessories/waydroid/install_waydroid_package_manager.sh
+++ accessories/waydroid/install_waydroid_package_manager.sh
@@ -3,6 +3,8 @@
 # Here we are always root and always are working on a virgin system
 #
 clear
+echo "============================================="
 echo "wardrobe: installing waydroid-package-manager"
+echo "============================================="
 read -t180 -rp "Press enter to continue..."
 scp -r artisan@192.168.1.2:/opt/waydroid/waydroid-package-manager /etc/skel
