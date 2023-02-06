47294ceb7f8dd61936d3dac08a0832900eb95cde
Piero Proietti <piero.proietti@gmail.com>
2023-02-06 11:15:40 +0100

    added themes from addons to wardrobe

---
+++ themes/linuxfx/patch/apply.sh
@@ -0,0 +1,15 @@
+#sudo cp linuxfx-mkdesktop.sh /usr/bin
+#cp linuxfx-mkdesktop.desktop ~/.config/autostart
+#sudo cp linuxfx-mkdesktop.desktop /etc/xdg/autostart
+#sudo cp linuxfx-mkdesktop.desktop /usr/share/applications
+
+sudo cp linuxfx-links-add.sh /usr/bin
+cp linuxfx-links-add.desktop ~/.config/autostart
+sudo cp linuxfx-links-add.desktop /etc/xdg/autostart
+sudo cp linuxfx-links-add.desktop /usr/share/applications
+
+cp xdg-user-dirs-update.desktop ~/.config/autostart
+sudo cp xdg-user-dirs-update.desktop /etc/xdg/autostart
+sudo cp xdg-user-dirs-update.desktop /usr/share/applications
+
+cp /etc/xdg/autostart/penguins-links-add.desktop ~/.config/autostart
