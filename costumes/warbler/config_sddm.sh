1fd917c4bbf1d9bb1f8052c61a68b90536de95e8
Piero Proietti <piero.proietti@gmail.com>
2023-02-07 14:24:59 +0100

    config-sddm plasma

--- costumes/warbler/config_sddm.sh
+++ costumes/warbler/config_sddm.sh
@@ -5,6 +5,6 @@ BACKGROUND=$(ls /usr/share/backgrounds/"${COSTUME}"/*.jpg)
 
 # set autologin
 # We need some different to just append 
-echo "# wardrobe\n[Autologin]\nUser=${SUDO_USER}\nSession=plasma-wayland\n" > /etc/sddm.conf
+echo "# wardrobe\n[Autologin]\nUser=${SUDO_USER}\nSession=plasma\n" > /etc/sddm.conf
 # set background
 #echo "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
