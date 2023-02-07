1e3b7b637653863f7c47cfd1d8812d9c994f6975
Piero Proietti <piero.proietti@gmail.com>
2023-02-07 14:02:16 +0100

    attualmente gli script di customize ricevono il nome del costume

--- costumes/warbler/config_sddm.sh
+++ costumes/warbler/config_sddm.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-USER=$1
-DESKTOP=$2
-BACKGROUND="/usr/share/backgrounds/helenae/25759889846_911ba6c1fd_o.jpg"
+COSTUME=$1
+BACKGROUND=$(ls /usr/share/backgrounds/"${COSTUME}"/*.jpg)
 
 # set autologin
 # We need some different to just append 
-echo "# wardrobe\n[Autologin]\nUser=${USER}\nSession=plasma-wayland\n" > /etc/sddm.conf
+echo "# wardrobe\n[Autologin]\nUser=${SUDO_USER}\nSession=plasma-wayland\n" > /etc/sddm.conf
 # set background
 #echo "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
