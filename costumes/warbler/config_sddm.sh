3ad424e658e7e31a48e70757c2f43056f70b6e3c
Piero Proietti <piero.proietti@gmail.com>
2023-02-07 11:17:30 +0100

    sddm

--- costumes/warbler/config_sddm.sh
+++ costumes/warbler/config_sddm.sh
@@ -4,10 +4,8 @@ USER=$1
 DESKTOP=$2
 BACKGROUND="/usr/share/backgrounds/helenae/25759889846_911ba6c1fd_o.jpg"
 
-
 # set autologin
 # We need some different to just append 
-echo "# wardrobe\n[Seat:*]\nautologin-user=${USER}" >> /etc/lightdm/lightdm.conf
-
+echo "# wardrobe\n[Autologin]\nUser=${USER}\nSession=plasma-wayland\n` > /etc/sddm.conf
 # set background
 echo "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
