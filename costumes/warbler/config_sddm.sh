d06940241a5100a1ef03c28b86c7dde411e95399
Piero Proietti <piero.proietti@gmail.com>
2023-02-07 11:54:18 +0100

    kde-config-sddm added

--- costumes/warbler/config_sddm.sh
+++ costumes/warbler/config_sddm.sh
@@ -8,4 +8,4 @@ BACKGROUND="/usr/share/backgrounds/helenae/25759889846_911ba6c1fd_o.jpg"
 # We need some different to just append 
 echo "# wardrobe\n[Autologin]\nUser=${USER}\nSession=plasma-wayland\n" > /etc/sddm.conf
 # set background
-echo "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
+#echo "# wardrobe\nbackground=${BACKGROUND}" >> /etc/lightdm/lightdm-gtk-greeter.conf
