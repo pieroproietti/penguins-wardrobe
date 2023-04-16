0029188a61c277fb2416677d5f6242b628ee4f14
Piero Proietti <piero.proietti@gmail.com>
2023-04-16 17:18:41 +0200

    eggs-dev

--- accessories/eggs-dev/ubuntu-jammy_eggs-dev.sh
+++ accessories/eggs-dev/ubuntu-jammy_eggs-dev.sh
@@ -8,6 +8,13 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# sources_list_d
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor -o /usr/share/keyrings/nodesource.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x bullseye main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
+
+# update
+sudo apt update 
+
 # install 
 sudo apt install --force-yes \
     nodejs \
