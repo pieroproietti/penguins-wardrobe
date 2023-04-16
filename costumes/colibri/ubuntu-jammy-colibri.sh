171da34c47e7af0d48951000e374f25c110ecae8
Piero Proietti <piero.proietti@gmail.com>
2023-04-16 05:41:32 -0400

    ubuntu

---
+++ costumes/colibri/ubuntu-jammy-colibri.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+clear
+echo "ubuntu-jammy-colibri"
+echo ""
+
+if [[ $EUID -ne 0 ]]; then
+   printf "%s need to run with root privileges. \nPlease, prefix it with sudo\n", "$0"
+   exit 1
+fi
+
+COSTUME="colibri"
+MY_USERNAME=$(logname)
+MY_USERHOME="/home/${MY_USERNAME}"
+# update
+
+# wait to start
+printf "wardrobe: Prepare your costume: %s?\n", "${COSTUME}"
+echo "Press enter to continue or CTRL-C to abort"
+read -r
+
+# install costume
+sudo apt install --force-yes \
+    adwaita-qt \
+    firefox \
+    libxfce4ui-utils  \
+    lightdm  \
+    lightdm-gtk-greeter  \
+    network-manager-gnome \
+    network-manager-openvpn \
+    network-manager-openvpn-gnome \
+    qt5ct  \
+    spice-vdagent  \
+    tango-icon-theme  \
+    thunar  \
+    xarchiver  \
+    xfce4-appfinder \
+    xfce4-notifyd \
+    xfce4-panel \
+    xfce4-pulseaudio-plugin \
+    xfce4-session  \
+    xfce4-settings \
+    xfce4-terminal \
+    xfce4-whiskermenu-plugin \
+    xfconf \
+    xfdesktop4 \
+    xfwm4 \
+
+# add user on autologin group
+#groupadd -r autologin
+#gpasswd -a "${MY_USERNAME}" autologin
+
+# accessories
+#source ../../accessories/eggs-dev/arch-wear-eggs-dev.sh
+
+# enabling services
+#systemctl enable NetworkManager
+#systemctl enable lightdm
+
+# copy configuration from dirs to / and MY_USERHOME
+cp ./dirs/* / -R
+rsync -avx ./dirs/etc/skel/.config "${MY_USERHOME}"/ 
+chown "${MY_USERNAME}:${MY_USERNAME}" "${MY_USERHOME}" -R
+
+# config lightdm $COSTUME
+../../scripts/config_lightdm.sh "${COSTUME}"
+../../scripts/config_desktop_link.sh
+
+# /etc/hostname
+echo ${COSTUME} > /etc/hostname
+
+# /etc/hosts
+cat << 'EOF' > /etc/hosts
+127.0.0.1 localhost localhost.localdomain
+127.0.1.1 colibri colibri.localhost 
+# The following lines are desirable for IPv6 capable hosts
+:: 1     ip6 - localhost ip6 - loopback
+fe00:: 0 ip6 - localnet
+ff00:: 0 ip6 - mcastprefix
+ff02:: 1 ip6 - allnodes
+ff02:: 2 ip6 - allrouters
+ff02:: 3 ip6 - allhosts
+EOF
+
+#reboot
+reboot
