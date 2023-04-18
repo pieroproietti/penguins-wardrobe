f6d9bd7b1b7abda82923608ec530dc0493be5d14
Piero Proietti <piero.proietti@gmail.com>
2023-04-18 09:31:00 +0200

    sed

--- costumes/chicks/ubuntu-jammy_chicks.sh
+++ costumes/chicks/ubuntu-jammy_chicks.sh
@@ -73,7 +73,7 @@ echo ${COSTUME} > /etc/hostname
 # /etc/hosts
 cat << 'EOF' > /etc/hosts
 127.0.0.1 localhost localhost.localdomain
-127.0.1.1 chicks chicks.localhost 
+127.0.1.1 COSTUME COSTUME.localhost 
 # The following lines are desirable for IPv6 capable hosts
 :: 1     ip6 - localhost ip6 - loopback
 fe00:: 0 ip6 - localnet
@@ -82,6 +82,8 @@ ff02:: 1 ip6 - allnodes
 ff02:: 2 ip6 - allrouters
 ff02:: 3 ip6 - allhosts
 EOF
+
+sed -i "'s/COSTUME/${COSTUME}'" /etc/hosts
 
 # reboot
 reboot
