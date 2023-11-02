a50b94890215441fa940203f876849c5038e25c4
Piero Proietti <piero.proietti@gmail.com>
2023-11-02 12:09:33 +0100

    config_lightdm.sh merlo

--- costumes/merlo/config_lightdm.sh
+++ costumes/merlo/config_lightdm.sh
@@ -6,6 +6,6 @@ BACKGROUND=$(ls /usr/share/backgrounds/"${COSTUME}"/*.jpg)
 # autologin
 LIGHTDM_CONF=/etc/lightdm/lightdm.conf
 if [ -f /etc/lightdm/lightdm.conf.d/lightdm-autologin-greeter.conf ]; then
-    # LIGHTDM_CONF=/etc/lightdm/lightdm.conf.d/lightdm-autologin-greeter.conf
+    LIGHTDM_CONF=/etc/lightdm/lightdm.conf.d/lightdm-autologin-greeter.conf
 fi
 echo -e "# wardrobe\n[Seat:*]\nautologin-user=${SUDO_USER}" >> ${LIGHTDM_CONF}
