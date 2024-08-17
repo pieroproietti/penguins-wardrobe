05c000c76fd450e1ed5ef8e2ff6adf95adf31246
Piero Proietti <piero.proietti@gmail.com>
2024-08-17 10:23:51 +0200

    doas

--- scripts/config_desktop_link.sh
+++ scripts/config_desktop_link.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# doas compatibile
+if [[ -n "$DOAS_USER" ]]; then
+    SUDO_USER="$DOAS_USER"
+fi
+
 DESKTOP=$(su "${SUDO_USER}" xdg-user-dir DESKTOP)
 
 # create DESKTOP
@@ -12,7 +17,7 @@ cp /usr/lib/penguins-eggs/assets/eggs.png /usr/share/icons
 
 # copy links
 cp -f /usr/lib/penguins-eggs/addons/eggs/adapt/applications/eggs-adapt.desktop "${DESKTOP}"
-cp -f/usr/lib/penguins-eggs/addons/eggs/adapt/bin/adapt /usr/bin/adapt
+cp -f /usr/lib/penguins-eggs/addons/eggs/adapt/bin/adapt /usr/bin/adapt
 chmod +x /usr/bin/adapt
 cp -f /usr/lib/penguins-eggs/assets/penguins-eggs.desktop "${DESKTOP}"
 chmod +x "${DESKTOP}"/*.desktop
