4c4e04b5a421affa3eda79298cf406e737cce896
Piero Proietti <piero.proietti@gmail.com>
2024-09-15 19:07:58 +0200

    cleaning

--- costumes/merlo/README.md
+++
@@ -1,7 +0,0 @@
-# merlo
-
-Created for my development needs, particularly for eggs, use mate.
-
-# background
-
-![merlo](./dirs/usr/share/backgrounds/merlo/merlo.jpg)
--- costumes/warbler/config_sddm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-COSTUME=$1
-BACKGROUND=$(ls /usr/share/backgrounds/"${COSTUME}"/*.jpg)
-
-# select plasma, not plasma-wayland becouse don't work autologin
-echo "# wardrobe\n[Autologin]\nUser=${SUDO_USER}\nSession=plasma\n" > /etc/sddm.conf
